30204e6e96d67e9d6ba19e24cc4aa7fb08b0b268
SimenHagen86 <jo.simen.hagen@gmail.com>
2022-11-16 09:51:10 +0100

    todo - need to find fault with button

---
+++ js/todoitem.js
@@ -0,0 +1,22 @@
+export default class ToDoItem {
+	consttructor() {
+		this._id = null
+		this._item = null
+	}
+
+	getId() {
+		return this._id
+	}
+
+	setId() {
+		this._id = id
+	}
+
+	getItem() {
+		return this._item
+	}
+
+	setItem() {
+		this._item = item
+	}
+}
